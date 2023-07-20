5a58397f6c262d871ecf00932c1f72759a6259c3
yuni0329 <yhchoi@arisys.co.kr>
2023-07-20 12:41:46 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int x = Integer.parseInt(str[0]);
+        int y = Integer.parseInt(str[1]);
+
+    }
+}
